edb556f2d37f9d12a269e54db2aa814803b9669a
nicolasholanda <nikolas.nhm14@gmail.com>
2026-03-20 08:20:57 -0300

    Initial commit

---
+++ src/main/java/com/logger/LoggerApplication.java
@@ -0,0 +1,12 @@
+package com.logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggerApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(LoggerApplication.class, args);
+    }
+}
