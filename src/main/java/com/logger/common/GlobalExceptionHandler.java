91f14506269bf886601479e63476593d09e9d907
nicolasholanda <nikolas.nhm14@gmail.com>
2026-03-25 09:47:59 -0300

    Added global exception handler

---
+++ src/main/java/com/logger/common/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.logger.common;
+
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleNotFound(RuntimeException ex) {
+        return Map.of("error", ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidation(MethodArgumentNotValidException ex) {
+        FieldError fieldError = ex.getBindingResult().getFieldErrors().get(0);
+        return Map.of("error", fieldError.getField() + ": " + fieldError.getDefaultMessage());
+    }
+}
